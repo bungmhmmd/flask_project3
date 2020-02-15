081d8554c953fca26539fe98a6c0cd4b8f588ddb
bungmhmmd <bungmohammad@gmail.com>
2020-02-15 15:28:02 +0700

    fourth commit all

--- model.py
+++ model.py
@@ -8,7 +8,9 @@ class Puppy(db.Model):
     age = db.Column(db.Integer)
     breed = db.Column(db.Text)
 
-    def __init__(self, name, age):
+    toys = db.relationship('Toy', backref='puppy', lazy='dynamic')
+
+    def __init__(self, name, age, breed):
         self.name = name
         self.age = age
         self.breed = breed
@@ -18,3 +20,20 @@ class Puppy(db.Model):
 
     def __repr__(self):
         return '<Puppy: {}>'.format(self.name)
+
+    def report_toys(self):
+        print("Here are my toys:")
+        for toy in self.toys:
+            print(toy.i_tem_name)
+
+class Toy(db.Model):
+
+    __tablename__ = 'toys'
+
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, item_name, puppy_id):
+        self.item_name = item_name
+        self.puppy_id = puppy_id
